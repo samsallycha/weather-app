caea4180695857c042d0411bec98df6e472692e1
Sam Sally Cha <samsallycha@gmail.com>
2021-03-21 16:51:22 +1100

    Updated search functionality
    Updates icon

--- src/script.js
+++ src/script.js
@@ -24,6 +24,9 @@ function showTemperature(response) {
   document.querySelector(".city").innerHTML = response.data.name;
 
   document.querySelector(".date").innerHTML = formatDate(response.data.dt * 1000);
+
+  document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
 }
 
 function formatDate(timestamp) {
